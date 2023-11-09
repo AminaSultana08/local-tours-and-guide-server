36a07f1a82087d424fee95fe8cd692d7845f9c16
AminaSultana08 <aminasultana088@gmail.com>
2023-11-09 09:44:18 +0600

    booking collection added to tour guide database

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
@@ -28,14 +28,21 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    //  await client.connect();
 
     const serviceCollection = client.db('tourGuide').collection('services')
+    const bookingCollection = client.db('tourGuide').collection('bookings')
 
 
     //read services
     app.get('/services', async(req,res)=>{
       try{
+        console.log(req.query.email);
+        let query = {}
+        if(req.query?.email){
+          query = {email : req.query.email}
+
+        }
         const cursor = serviceCollection.find()
         const result = await cursor.toArray()
         res.send(result)
@@ -45,6 +52,27 @@ async function run() {
         console.log(error);
       }
     })
+
+    //singleService
+    app.get('/services/:id', async(req,res)=>{
+      try{
+        const id = req.params.id
+        const query = {_id : new ObjectId(id)}
+        const options = {
+         
+          // Include only the `title` and `imdb` fields in the returned document
+          projection: { address: 1,description:1,email:1,photo:1,price:1 , providerName:1,serviceName:1 },
+        };
+    
+        const result = await serviceCollection.findOne(query,options)
+        res.send(result)
+      }
+      catch (error) {
+        console.log(error);
+      }
+    })
+
+
    //add services
    
    app.post('/services',async(req,res)=>{
